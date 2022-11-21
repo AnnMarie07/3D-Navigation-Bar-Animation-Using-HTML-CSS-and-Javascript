11ac879b4b0a51f6e65d5679272f09f40a90784a
unknown <annmarieaddatu@gmail.com>
2022-11-21 16:07:11 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const portfolio_menu = document.querySelector(".portfolio-menu");
+const container = document.querySelector(".container");
+
+portfolio_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+/*COPYRIGHT Youtube Channel True Coder*/
